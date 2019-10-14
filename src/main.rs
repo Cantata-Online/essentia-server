65ecfcfd3225e392e5bcf2d34c0cd2ceca5894fa
Sergei Beljakov <1899416+tasmanianfox@users.noreply.github.com>
2019-10-15 00:19:05 +0300

    Command handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod network;
 
 use env_logger::Env;
-use log::{info, debug};
+use log::{info, debug, error};
 
-use std::time::Duration;
-use std::thread;
+use std::io;
 
 use network::server_udp::{Listener};
 
@@ -12,15 +11,31 @@ fn init_logger() {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     init_logger();
 
     let mut server = Listener::create();
     server.start();
     info!("Server started.");
 
-    loop {
-        thread::sleep(Duration::from_millis(5000));
-        debug!("Running...");
+    let mut do_exit:bool = false;
+    while !do_exit {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_n) => {
+                input = String::from(input.trim());
+                match input.trim().as_ref() {
+                    "quit" => {
+                        do_exit = true;
+                    },
+                    _ => error!("Unknown command: {}", input)
+                }
+            }
+            Err(error) => error!("{}", error),
+        }
     }
+    debug!("Exited from main loop");
+    info!("Server terminated.");
+
+    Ok(())
 }
