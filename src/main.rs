2269fb72154356128881ddd9297c38c4d0da5862
Sergei Beljakov <1899416+tasmanianfox@users.noreply.github.com>
2019-10-17 22:24:26 +0300

    Replaced CLI handler routines from main function

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,23 @@
 mod network;
+mod system;
 
 use env_logger::Env;
-use log::{info, debug, error};
-
+use log::{info, debug};
 use std::io;
 
 use network::server_udp::{Listener};
+use system::cli_handler::{cli_handler};
 
-fn init_logger() {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
-}
 
 fn main() -> io::Result<()> {
-    init_logger();
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
 
     let mut server = Listener::create();
     server.start();
     info!("Server started.");
 
-    let mut do_exit:bool = false;
-    while !do_exit {
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_n) => {
-                input = String::from(input.trim());
-                match input.trim().as_ref() {
-                    "quit" => {
-                        do_exit = true;
-                    },
-                    _ => error!("Unknown command: {}", input)
-                }
-            }
-            Err(error) => error!("{}", error),
-        }
-    }
+    cli_handler();
+
     debug!("Exited from main loop");
     info!("Server terminated.");
 
---
+++ src/system/cli_handler.rs
@@ -0,0 +1,36 @@
+use std::io;
+use log::{error};
+
+fn command_handler(input: &str) -> bool {
+    let mut result = false;
+    match input.trim().as_ref() {
+        "quit" => {
+            result = true;
+        },
+        _ => error!("Unknown command: {}", input)
+    }
+    result
+}
+
+/// Handles an input from command line.
+/// Returns `true` if the main loop should be interrupted
+fn main_iteration() -> bool {
+    let mut input = String::new();
+    let mut result = false;
+    match io::stdin().read_line(&mut input) {
+        Ok(_n) => {
+            let input = input.trim();
+            result = command_handler(input)
+        }
+        Err(error) => error!("{}", error),
+    }
+    result
+}
+
+/// Handles command line input
+pub fn cli_handler() {
+    let mut do_exit:bool = false;
+    while !do_exit {
+        do_exit = main_iteration();
+    }
+}
